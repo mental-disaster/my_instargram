39afdaf0e55edd4bdeec9c0a5621c2182ce5509a
mental_disaster <ghgh9244@gmail.com>
2022-01-19 23:08:47 +0900

    update signup feature

--- my_instargram/users/forms.py
+++ my_instargram/users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django import forms as django_forms
 
 User = get_user_model()
 
@@ -17,3 +18,30 @@ class UserCreationForm(admin_forms.UserCreationForm):
         error_messages = {
             "username": {"unique": _("This username has already been taken.")}
         }
+
+
+class SignUpForm(django_forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password']
+
+        labels = {
+            'email': '이메일 주소',
+            'name' : '성명',
+            'username' : '사용자 이름',
+            'password' : '비밀번호',
+        }
+
+        widgets = {
+            'email': django_forms.TextInput(attrs={'placeholder': 'example@email.com'}),
+            'name': django_forms.TextInput(attrs={'placeholder': 'Doe'}),
+            'username': django_forms.TextInput(attrs={'placeholder': 'ID'}),
+            'password' : django_forms.PasswordInput(attrs={'placeholder': 'password'}),
+        }
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_apssword(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
