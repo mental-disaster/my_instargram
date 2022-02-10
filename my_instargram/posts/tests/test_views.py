bcd350e4b61790a78da8087015934f400c51dd7e
mental_disaster <ghgh9244@gmail.com>
2022-02-10 13:18:36 +0900

    working on post function and test file

---
+++ my_instargram/posts/tests/test_views.py
@@ -0,0 +1,12 @@
+from audioop import reverse
+from django.urls import reverse
+from django.test import TestCase
+
+class TestPosts(TestCase):
+
+    def test_get_posts_page(self):
+        url = reverse('posts:post_create')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'posts/post_create.html')
--- my_instargram/posts/urls.py
+++ my_instargram/posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create/', views.post_create, name='post_create'),
 ]
